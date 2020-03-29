de32b9ba65e064078581b1be03bf044fe2f361ca
Jessica Mercer <jessica.mercer@hotmail.com>
2020-03-29 22:21:03 +0100

    image test

--- src/app/index.js
+++ src/app/index.js
@@ -22,7 +22,7 @@ export default () => (
       <Header />
       <Route path={routes.home} exact component={Home} />
       <Route path={`${routes.project}/:slug`} component={Home} />
-      <Route path={routes.contact} exact component={() => <div>contact test</div>} />
+      <Route path={routes.contact} exact component={() => <div>contact hey</div>} />
     </HashRouter>
   </Provider>
 );
