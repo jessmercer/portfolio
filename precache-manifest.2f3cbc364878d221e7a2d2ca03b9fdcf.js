7525d23e356801636d5808401722fb039333b096
jessmercer <jessica.mercer@hotmail.com>
2020-03-29 21:18:14 +0000

    Deploying to gh-pages from  - 79187e631c179e9ce6368be529c0a366066fa18f 🚀

--- precache-manifest.2f3cbc364878d221e7a2d2ca03b9fdcf.js
+++ precache-manifest.2f3cbc364878d221e7a2d2ca03b9fdcf.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b660446dcee4a3cf2077ed5428cf3f92",
+    "revision": "44fe18a495bf3a8c6968722895809cdd",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "2a6209e869a9931bf6bd",
+    "revision": "67a297bb7ab6f406164b",
     "url": "/portfolio/static/css/main.472a71b9.chunk.css"
   },
   {
-    "revision": "188305bab4e2b788136f",
-    "url": "/portfolio/static/js/2.37dd0bb9.chunk.js"
+    "revision": "230811f74e3175bce950",
+    "url": "/portfolio/static/js/2.cc00b2f0.chunk.js"
   },
   {
     "revision": "53b91fb6912e14c9689b5be87306d93a",
-    "url": "/portfolio/static/js/2.37dd0bb9.chunk.js.LICENSE"
+    "url": "/portfolio/static/js/2.cc00b2f0.chunk.js.LICENSE"
   },
   {
-    "revision": "2a6209e869a9931bf6bd",
-    "url": "/portfolio/static/js/main.d4c80bdc.chunk.js"
+    "revision": "67a297bb7ab6f406164b",
+    "url": "/portfolio/static/js/main.d2cb0549.chunk.js"
   },
   {
     "revision": "0d98471f6c3b69ca4251",
