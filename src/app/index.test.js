1d206bb1ba1a8e291360777746d2081c8e632cc8
jessmercer <jessica.mercer@hotmail.com>
2020-03-29 17:47:58 +0000

    Deploying to gh-pages from  - 8f6851d7655572fdec6364ee4b748d19f9302bc9 🚀

--- src/app/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '.';
-
-it('renders the app', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
