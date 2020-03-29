6be29af1c0a46d85a126fa6c06bc2a25363cdefc
jessmercer <jessica.mercer@hotmail.com>
2020-03-29 18:03:01 +0000

    Deploying to gh-pages from  - 5c36ea8cf66052f76eff776bf12bf49106926ae8 🚀

--- precache-manifest.dd6bd638ea4baa3c584a946cf0a2427e.js
+++ precache-manifest.dd6bd638ea4baa3c584a946cf0a2427e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5a9cdadee50789e1dc790b9ee19f5a91",
+    "revision": "55050826f1b107d00468575ab80e8386",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "9624834e04613a8102f2",
+    "revision": "21d05ad3b35df70ea29f",
     "url": "/portfolio/static/css/main.472a71b9.chunk.css"
   },
   {
-    "revision": "54a81e78eec870b9b9ab",
-    "url": "/portfolio/static/js/2.ac214429.chunk.js"
+    "revision": "188305bab4e2b788136f",
+    "url": "/portfolio/static/js/2.37dd0bb9.chunk.js"
   },
   {
     "revision": "53b91fb6912e14c9689b5be87306d93a",
-    "url": "/portfolio/static/js/2.ac214429.chunk.js.LICENSE"
+    "url": "/portfolio/static/js/2.37dd0bb9.chunk.js.LICENSE"
   },
   {
-    "revision": "9624834e04613a8102f2",
-    "url": "/portfolio/static/js/main.4d435940.chunk.js"
+    "revision": "21d05ad3b35df70ea29f",
+    "url": "/portfolio/static/js/main.becb533b.chunk.js"
   },
   {
     "revision": "0d98471f6c3b69ca4251",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.59f0481996daaf46c55c4a119b80b881.js"
+  "/portfolio/precache-manifest.dd6bd638ea4baa3c584a946cf0a2427e.js"
 );
 
 self.addEventListener('message', (event) => {
